23e98db81b9ed8f70fea00e5ce796ea8d19f304b
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-09-25 23:59:47 +0800

    Refine footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,13 +5,13 @@ const Footer: FC = () => {
   return (
     <footer className="px-4 sm:px-20">
       <hr />
-      <div className="mt-6 flex flex-col gap-0 items-center text-pd-gray text-sm">
+      <div className="mt-6 flex flex-col gap-0 items-center text-pd-gray-400 text-sm">
         <div>© 2023 github/produce101jpthegirls</div>
         <div className="text-center">
-          <span className="inline-block">This site is a fan made site.</span>
-          <span className="inline-block">{`It's not made by the official team.`}</span>
+          <span className="inline-block">This is a fan made site.</span>
+          <span className="inline-block">{`It's not created by the official team.`}</span>
         </div>
-        <div className="mt-4 flex gap-4">
+        <div className="mt-4 mb-8 flex gap-4">
           <Link
             className="hover:text-pd-pink-400 underline"
             href="https://produce101.jp/"
