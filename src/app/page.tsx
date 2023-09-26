0025c2a16677fc60f8af2cf4ddcc48eff5016140
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-09-26 19:10:41 +0800

    Refine content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,10 +72,17 @@ export default function Home({ params, searchParams }: {
   return (
     <main className={`${noto_sans_jp.className} h-full`}>
       <Header />
-      <div
-        className="my-12 sm:my-20 px-4 text-pd-gray-300 text-center text-xs sm:text-base"
-      >
-        初めて羽ばたいた101人の少女たちが<br className="sm:hidden" />デビューを目指して集ました<br /><br className="sm:hidden" />夢の花道に向かって駆け抜ける<br className="sm:hidden" />彼女たちを応援していきましょう
+      <div className="my-12 sm:my-20 px-4 text-center">
+        <h2
+          className="text-pd-pink-100 text-sm sm:text-xl font-bold mb-6"
+        >PRODUCE 101 JAPAN THE GIRLS RANKER
+        </h2>
+        <div
+          className="text-pd-gray-300 text-center text-xs sm:text-base"
+        >
+          初めて羽ばたいた101人の少女たちが<br className="sm:hidden" />デビューを目指して集ました<br />
+          <br className="sm:hidden" />夢の花道に向かって駆け抜ける<br className="sm:hidden" />彼女たちを応援していきましょう
+        </div>
       </div>
       <div
         className="my-4 sm:my-20 px-4 text-pd-gray-400 flex flex-col sm:flex-row gap-0 sm:gap-8
