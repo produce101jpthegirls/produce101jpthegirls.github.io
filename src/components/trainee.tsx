08994d4f7b59aa5dd237807a6867424eb1ba8a59
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-09-26 21:09:48 +0800

    Random btn and Hover flip animation

--- src/components/trainee.tsx
+++ src/components/trainee.tsx
@@ -336,9 +336,24 @@ export const SelectionView: FC<SelectionViewProps> = ({ selected, setSelected })
     <>
       <div className="px-4 py-[0.85rem] sm:py-[0.96rem] border-b flex justify-between items-center">
         <div className={`${selectionCompleted ? "text-pd-pink-400" : "text-pd-pink-100"} font-bold text-base sm:text-base`}>SHARE YOUR TOP 11</div>
-        <div>
+        <div className="flex items-center">
           <button
-            className={`ml-3 ${disabled ? "text-gray-200" : "text-pd-pink-400 hover:text-pd-pink-100"}`}
+            className="ml-3 mr-1 text-pd-pink-400 group"
+            onClick={() => {
+              const unshuffled = Array.from(Array(trainees.length).keys());
+              const shuffled = unshuffled
+                .map(value => ({ value, sort: Math.random() }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({ value }) => value)
+              setSelected(shuffled.slice(0, 11));
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mt-0.5 transition duration-300 group-hover:flip-y">
+              <path d="M18 9v-3c-1 0-3.308-.188-4.506 2.216l-4.218 8.461c-1.015 2.036-3.094 3.323-5.37 3.323h-3.906v-2h3.906c1.517 0 2.903-.858 3.58-2.216l4.218-8.461c1.356-2.721 3.674-3.323 6.296-3.323v-3l6 4-6 4zm-9.463 1.324l1.117-2.242c-1.235-2.479-2.899-4.082-5.748-4.082h-3.906v2h3.906c2.872 0 3.644 2.343 4.631 4.324zm15.463 8.676l-6-4v3c-3.78 0-4.019-1.238-5.556-4.322l-1.118 2.241c1.021 2.049 2.1 4.081 6.674 4.081v3l6-4z" />
+            </svg>
+          </button>
+          <button
+            className={`ml-3 ${disabled ? "text-gray-200" : "text-pd-pink-400 group"}`}
             disabled={disabled}
             onClick={() => {
               navigator.clipboard.writeText(window.location.href)
@@ -347,13 +362,13 @@ export const SelectionView: FC<SelectionViewProps> = ({ selected, setSelected })
                 });
             }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 transition duration-300 group-hover:flip-y">
               <path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
               <path d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
             </svg>
           </button>
           <button
-            className={`ml-3 ${disabled ? "text-gray-200" : "text-pd-pink-400 hover:text-pd-pink-100"}`}
+            className={`ml-3 ${disabled ? "text-gray-200" : "text-pd-pink-400 group"}`}
             disabled={disabled}
             onClick={() => {
               const tmp = createDownloadSelection();
@@ -387,7 +402,7 @@ export const SelectionView: FC<SelectionViewProps> = ({ selected, setSelected })
               }
             }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 transition duration-300 group-hover:flip-y">
               <path d="M12 1.5a.75.75 0 01.75.75V7.5h-1.5V2.25A.75.75 0 0112 1.5zM11.25 7.5v5.69l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V7.5h3.75a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9a3 3 0 013-3h3.75z" />
             </svg>
           </button>
