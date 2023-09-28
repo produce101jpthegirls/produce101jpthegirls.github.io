c9c435f5dbaee314559086bde32dff4293fc2fd4
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-09-28 17:33:24 +0800

    Add trainee type

---
+++ src/components/types.ts
@@ -0,0 +1,12 @@
+type Trainee = {
+  index: number;
+  id: string;
+  code: string;
+  nameEn: string;
+  nameJp: string;
+  birthday: string;
+  birthPlace: string;
+  mbtiType: string;
+  profileUrl: string;
+  videoUrl: string;
+};
