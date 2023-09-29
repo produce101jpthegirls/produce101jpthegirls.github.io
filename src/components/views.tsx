1b1419b92688eb257a58de484b2854c9d762d3c5
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-09-29 22:30:29 +0800

    Add icons

--- src/components/views.tsx
+++ src/components/views.tsx
@@ -203,24 +203,32 @@ const ListView: FC<ListViewProps> = ({ items, selected, setSelected }) => {
               </div>
               <div className="select-none">{item.id}</div>
             </div>
-            <div className="sm:mt-0.5 flex justify-between items-center">
-              <div className="flex gap-4 items-center">
-                <span className="select-none">{item.birthday}</span>
-                <span className="select-none">{item.birthPlace}</span>
-                <span className="select-none">{item.mbtiType}</span>
-              </div>
-              <Link
-                className="text-pd-gray-300 hover:text-pd-pink-400 sm:font-medium text-sm"
-                href={item.profileUrl}
-                target="_blank"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <span className="sm:inline after:content-['_↗'] after:text-xs after:font-bold">Profile</span>
-              </Link>
+            <div className="sm:mt-0.5 flex gap-4 items-center">
+              <span className="select-none">{item.birthday}</span>
+              <span className="select-none">{item.birthPlace}</span>
+              <span className="select-none">{item.mbtiType}</span>
             </div>
             <div className="sm:mt-0.5 flex justify-between items-end text-sm">
-              <span>VIDEOS</span>
-              <div className="flex gap-4 items-center">
+              <div className="flex gap-3 item-centers">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 mt-[2.5px] text-pd-pink-100 hidden sm:inline">
+                  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                </svg>
+                <Link
+                  className="text-pd-gray-300 hover:text-pd-pink-400 sm:font-medium text-sm"
+                  href={item.profileUrl}
+                  target="_blank"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <span className="sm:inline after:content-['_↗'] after:text-xs after:font-bold">Profile</span>
+                </Link>
+              </div>
+              <div className="flex gap-3 item-centers">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461.001 461.001" fill="currentColor" className="w-4 h-4 mt-[2.5px] text-pd-pink-100 hidden sm:inline">
+                  <path d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
+                    c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
+                    C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
+                    c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"/>
+                </svg>
                 <Link
                   className="text-pd-gray-300 hover:text-pd-pink-400 sm:font-medium"
                   href={item.videoUrls.pr}
