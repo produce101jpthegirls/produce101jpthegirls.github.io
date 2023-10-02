884d7d60ce8e7f846cd2a24d7d6a798061061d92
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-10-03 02:17:55 +0800

    Refine title and description

---
+++ src/app/characteristics/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import RootLayout from "../layout";
+
+export const metadata: Metadata = {
+  title: "CHARACTERISTICS | PRODUCE 101 JAPAN THE GIRLS RANKER",
+  description: "CHARACTERISTICS | PRODUCE 101 JAPAN THE GIRLS RANKER",
+  openGraph: {
+    title: "CHARACTERISTICS | PRODUCE 101 JAPAN THE GIRLS RANKER",
+    description: "CHARACTERISTICS | PRODUCE 101 JAPAN THE GIRLS RANKER",
+    url: "https://produce101jpthegirls.github.io/characteristics",
+    siteName: "PRODUCE 101 JAPAN THE GIRLS RANKER",
+    type: "website",
+    locale: "en-US",
+    images: [
+      {
+        url: "https://produce101jpthegirls.github.io/og_preview.png",
+        width: 800,
+        height: 600,
+        alt: "Preview",
+      },
+    ],
+  },
+};
+
+export default RootLayout;
