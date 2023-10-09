8a1e73ee56fcbeb070bdab482da1fcad6b016421
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-10-09 20:44:18 +0800

    Fix view count sort

--- src/components/tables.tsx
+++ src/components/tables.tsx
@@ -1,7 +1,7 @@
 import { TRAINEES } from "@/constants";
 import { useSiteContext } from "@/context/site";
 import { CONTENTS } from "@/i18n";
-import { formatHumanNumber } from "@/utils";
+import { formatHumanNumber, isCompletedSelection } from "@/utils";
 import Link from "next/link";
 import { FC } from "react";
 import DataTable, { TableColumn, TableStyles } from "react-data-table-component";
@@ -52,6 +52,7 @@ export type ViewCountDataRow = {
 
 type DetailedAnalyticsDataRow = {
   trainee: AnalyticsTrainee;
+  level_division: AnalyticsVideo;
   speed_eating: AnalyticsVideo;
   eye_contact: AnalyticsVideo;
   pr: AnalyticsVideo;
@@ -102,9 +103,8 @@ type TopNDataTableProps = {
 
 export const TopNDataTable: FC<TopNDataTableProps> = ({ pending, data, n, filterSelected, title }) => {
   const { selected, language } = useSiteContext();
-  const selectedTraineeIds = selected.map((index) => TRAINEES[index].code);
-  const filteredData = filterSelected ? (
-    data.filter((row) => row.trainees.some((trainee) => selectedTraineeIds.includes(trainee.id)))
+  const filteredData = filterSelected && isCompletedSelection(selected) ? (
+    data.filter((row) => row.trainees.some((trainee) => selected.map((index) => TRAINEES[index].code).includes(trainee.id)))
   ) : data;
   // const sortedData = [...filteredData].sort((a, b) => a.rank - b.rank);
   const topNData = filteredData.slice(0, n);
@@ -143,6 +143,23 @@ export const TopNDataTable: FC<TopNDataTableProps> = ({ pending, data, n, filter
   );
 };
 
+type TraineeIconsProps = {
+  trainees: AnalyticsTrainee[];
+};
+
+const TraineeIcons: FC<TraineeIconsProps> = ({ trainees }) => {
+  return (
+    <div className="mt-1 flex gap-1">
+      {trainees.map((trainee) => {
+        const img = trainee.img ? trainee.img : { "src": "", "alt": "" };
+        return (
+          <img key={trainee.id} className="rounded-full w-7 h-7 sm:w-8 sm:h-8 sm:hidden" src={img.src} alt={img.alt} />
+        );
+      })}
+    </div>
+  );
+};
+
 const renderCustomName = (trainees: AnalyticsTrainee[], customName: string) => {
   if (customName.includes(" ✧ ")) {
     const [team, artistSong] = customName.split(" ✧ ");
@@ -150,38 +167,26 @@ const renderCustomName = (trainees: AnalyticsTrainee[], customName: string) => {
       <div>
         <div className="font-bold">{artistSong}</div>
         <div>{team}</div>
-        <div className="mt-1 flex gap-1">
-          {trainees.map((trainee) => {
-            const img = trainee.img ? trainee.img : { "src": "", "alt": "" };
-            return (
-              <img className="rounded-full w-7 h-7 sm:w-8 sm:h-8" src={img.src} alt={img.alt} />
-            );
-          })}
-        </div>
+        <TraineeIcons trainees={trainees} />
       </div>
     );
   }
   return (
     <>
       <div className="flex items-center gap-1">
-        {trainees.map((trainee) => {
-          const img = trainee.img ? trainee.img : { "src": "", "alt": "" };
-          return (
-            <img className="rounded-full w-8 h-8 hidden sm:block" src={img.src} alt={img.alt} />
-          );
-        })}
+        <TraineeIcons trainees={trainees} />
         <span className="pl-1">{customName}</span>
       </div>
-      <div className="mt-1 flex gap-1">
-        {trainees.map((trainee) => {
-          const img = trainee.img ? trainee.img : { "src": "", "alt": "" };
-          return (
-            <img className="rounded-full w-7 h-7 sm:w-8 sm:h-8 sm:hidden" src={img.src} alt={img.alt} />
-          );
-        })}
-      </div>
+      <TraineeIcons trainees={trainees} />
     </>
   );
+};
+
+const getViewCountNumber = (viewCountStr: string): number => {
+  if (viewCountStr) {
+    return parseInt(viewCountStr);
+  }
+  return 0;
 }
 
 type TraineesCellProps = {
@@ -212,22 +217,24 @@ const TraineesCell: FC<TraineesCellProps> = ({ trainees, customName }) => {
 };
 
 type ViewCountCellProps = {
-  viewCount: string;
-  videoId: string;
+  viewCount?: string;
+  videoId?: string;
   rank?: number;
 };
 
 const ViewCountCell: FC<ViewCountCellProps> = ({ viewCount, videoId, rank }) => (
   <div className="flex gap-2 sm:gap-4">
-    <span className="w-[36px]">{formatHumanNumber(parseInt(viewCount), 0)}</span>
-    <Link className="text-pd-gray-300 hover:text-pd-pink-100" href={`https://youtu.be/${videoId}`} target="_blank">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461.001 461.001" fill="currentColor" className="w-4 h-4 mt-[2.5px]">
-        <path d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
-            c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
-            C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
-            c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"/>
-      </svg>
-    </Link>
+    <span className="w-[36px]">{viewCount ? formatHumanNumber(parseInt(viewCount), 0) : ""}</span>
+    {videoId && (
+      <Link className="text-pd-gray-300 hover:text-pd-pink-100" href={`https://youtu.be/${videoId}`} target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461.001 461.001" fill="currentColor" className="w-4 h-4 mt-[2.5px]">
+          <path d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
+              c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
+              C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
+              c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"/>
+        </svg>
+      </Link>
+    )}
     {rank && rank <= 5 && (
       <span>{rank}</span>
     )}
@@ -248,6 +255,7 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
   if (data) {
     console.log(data)
     const columnData = [
+      data.level_division,
       data.speed_eating,
       data.eye_contact,
       data.pr,
@@ -262,7 +270,7 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
       return newTable;
     });
   
-    const filteredTrainees = filterSelected ? selected.map((index) => TRAINEES[index]) : TRAINEES;
+    const filteredTrainees = filterSelected && isCompletedSelection(selected) ? selected.map((index) => TRAINEES[index]) : TRAINEES;
   
     detailedData = filteredTrainees.map((trainee) => ({
       trainee: {
@@ -271,10 +279,14 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
         nameJp: trainee.nameJp,
         img: getThumbnail(trainee.code, trainee.nameEn),
       },
-      speed_eating: columnData[0][trainee.code],
-      eye_contact: columnData[1][trainee.code],
-      pr: columnData[2][trainee.code],
-      fancam: columnData[3][trainee.code],
+      level_division: columnData[0][trainee.code] ?? ({
+        viewCount: undefined,
+        id: undefined,
+      }),
+      speed_eating: columnData[1][trainee.code],
+      eye_contact: columnData[2][trainee.code],
+      pr: columnData[3][trainee.code],
+      fancam: columnData[4][trainee.code],
     }));
   }
 
@@ -288,13 +300,22 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
       sortFunction: (a, b) => parseInt(a.trainee.id.split("_")[0]) - parseInt(b.trainee.id.split("_")[0]),
     },
     {
-      name: CONTENTS[language]["analytics"]["detailsTab"]["table"]["columns"][5],
+      name: CONTENTS[language]["analytics"]["detailsTab"]["table"]["columns"][6],
       cell: (row: DetailedAnalyticsDataRow) => (
         <ViewCountCell viewCount={row.speed_eating.viewCount} videoId={row.speed_eating.id} />
       ),
       minWidth: "60px",
       sortable: true,
-      sortFunction: (a, b) => parseInt(b.speed_eating.viewCount) - parseInt(a.speed_eating.viewCount),
+      sortFunction: (a, b) => getViewCountNumber(b.speed_eating.viewCount) - getViewCountNumber(a.speed_eating.viewCount),
+    },
+    {
+      name: CONTENTS[language]["analytics"]["detailsTab"]["table"]["columns"][5],
+      cell: (row: DetailedAnalyticsDataRow) => (
+        <ViewCountCell viewCount={row.level_division.viewCount} videoId={row.level_division.id} />
+      ),
+      minWidth: "60px",
+      sortable: true,
+      sortFunction: (a, b) => getViewCountNumber(b.level_division.viewCount) - getViewCountNumber(a.level_division.viewCount),
     },
     {
       name: CONTENTS[language]["analytics"]["detailsTab"]["table"]["columns"][4],
@@ -303,7 +324,7 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
       ),
       minWidth: "60px",
       sortable: true,
-      sortFunction: (a, b) => parseInt(b.eye_contact.viewCount) - parseInt(a.eye_contact.viewCount),
+      sortFunction: (a, b) => getViewCountNumber(b.eye_contact.viewCount) - getViewCountNumber(a.eye_contact.viewCount),
     },
     {
       name: CONTENTS[language]["analytics"]["detailsTab"]["table"]["columns"][3],
@@ -312,7 +333,7 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
       ),
       minWidth: "60px",
       sortable: true,
-      sortFunction: (a, b) => parseInt(b.pr.viewCount) - parseInt(a.pr.viewCount),
+      sortFunction: (a, b) => getViewCountNumber(b.pr.viewCount) - getViewCountNumber(a.pr.viewCount),
     },
     {
       name: CONTENTS[language]["analytics"]["detailsTab"]["table"]["columns"][2],
@@ -321,7 +342,7 @@ export const DetailedDataTable: FC<DetailedDataTableProps> = ({ pending, data, f
       ),
       minWidth: "60px",
       sortable: true,
-      sortFunction: (a, b) => parseInt(b.fancam.viewCount) - parseInt(a.fancam.viewCount),
+      sortFunction: (a, b) => getViewCountNumber(b.fancam.viewCount) - getViewCountNumber(a.fancam.viewCount),
     },
   ];
 
