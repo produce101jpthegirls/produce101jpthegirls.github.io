b9432e582ec17462a1f7988d0068bd55e8bc5994
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-10-20 21:45:29 +0800

    Fix news bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
               >
                 {newsItems.map((item, index) => (
                   item.stableLink ? (
-                    <StableLink key={index} className="text-sm sm:text-base text-pd-gray-400 hover:text-pd-pink-100 flex gap-2 items-center" pathname={item.stableLink}>
+                    <StableLink key={index} className="text-sm sm:text-base text-pd-gray-400 hover:text-pd-pink-100 flex gap-2 items-center min-h-[24px]" pathname={item.stableLink}>
                       <span className="sm:hidden">{item.shortText}</span>
                       <span className="hidden sm:inline">{item.longText}</span>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hidden sm:block">
@@ -84,12 +84,9 @@ export default function Home() {
                       </svg>
                     </Link>
                   ) : (
-                    <div key={index}>
+                    <div key={index} className="text-sm sm:text-base text-pd-gray-400 flex gap-2 items-center">
                       <span className="sm:hidden">{item.shortText}</span>
                       <span className="hidden sm:inline">{item.longText}</span>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hidden sm:block">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
-                      </svg>
                     </div>
                   )
                 ))}
